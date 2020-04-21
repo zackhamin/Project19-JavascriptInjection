819dec717a6464ec2efbead5fb88b149e935e592
Zack <ishaqamincoop@icloud.com>
2020-04-21 10:38:11 +0100

    First commit

---
+++ Project-19-JavascriptInjection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project-19-JavascriptInjection
+//
+//  Created by Ishaq Amin on 21/04/2020.
+//  Copyright © 2020 Ishaq Amin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
